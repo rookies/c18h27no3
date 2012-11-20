e7edefc9fa75eb1d390c97f99f38789d0258a58d
Robert <robert@privatdemail.net>
2012-11-20 21:21:27 +0100

    updated docs for gamemode adding

---
+++ docs/gamemode.tpl.cpp
@@ -0,0 +1,47 @@
+/*
+ * FILEHEADER
+ */
+#include "GAMEMODE.hpp"
+
+CLASSNAME::CLASSNAME()
+{
+
+}
+CLASSNAME::~CLASSNAME()
+{
+
+}
+int CLASSNAME::init(Config conf)
+{
+	/*
+	 * ...
+	*/
+	return 0;
+}
+int CLASSNAME::uninit(void)
+{
+	/*
+	 * ...
+	*/
+	return 0;
+}
+int CLASSNAME::calculate_sizes(int w, int h)
+{
+	/*
+	 * ...
+	*/
+	return 0;
+}
+void CLASSNAME::process_event(sf::Event event, int mouse_x, int mouse_y, EventProcessorReturn *ret)
+{
+	/*
+	 * ...
+	*/
+}
+UniversalDrawableArray CLASSNAME::get_drawables(void)
+{
+
+	/*
+	 * ...
+	*/
+}
---
+++ docs/gamemode.tpl.hpp
@@ -0,0 +1,37 @@
+/*
+ * FILEHEADER
+ */
+#ifndef CLASSNAME_HPP
+#	define CLASSNAME_HPP
+
+#	include "../gamemode.hpp"
+#	include "../universal_drawable.hpp"
+#	include "../event_processor_return.hpp"
+	
+	class CLASSNAME : public Gamemode
+	{
+		public:
+			CLASSNAME();
+			virtual ~CLASSNAME();
+			
+			/*
+			 * (Un)init:
+			*/
+			int init(Config conf); /* Called in Game::init_gamemode() */
+			int uninit(void); /* Called in Game::uninit_gamemode() */
+			/*
+			 * Calculate sizes:
+			*/
+			int calculate_sizes(int w, int h); /* Called in Game::calculate_sizes() */
+			/*
+			 * Process events:
+			*/
+			void process_event(sf::Event event, int mouse_x, int mouse_y, EventProcessorReturn *ret); /* Called in Game::process_events() */
+			/*
+			 * Get drawable items:
+			*/
+			UniversalDrawableArray get_drawables(void); /* Called in Game::draw() */
+		private:
+			
+	};
+#endif // CLASSNAME_HPP
