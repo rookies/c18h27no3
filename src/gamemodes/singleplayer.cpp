9d3ec86941fbd230c22bfa1f1d24cac1b5d32371
Robert <robert@privatdemail.net>
2013-05-12 16:08:28 +0200

    cleanup in singleplayer gamemode

--- src/gamemodes/singleplayer.cpp
+++ src/gamemodes/singleplayer.cpp
@@ -1,5 +1,24 @@
 /*
- * FILEHEADER
+ * singleplayer.cpp
+ * 
+ * Copyright 2013 Robert Knauer <robert@privatdemail.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
  */
 #include "singleplayer.hpp"
 
@@ -37,7 +56,6 @@ int SinglePlayer::init(Config conf, std::string arg)
 	/*
 	 * Load level from file:
 	*/
-	std::cout << arg << std::endl;
 	if (arg.compare("") == 0)
 	{
 		if (!m_level.load_from_file("level1.dat"))
