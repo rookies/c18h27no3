574cf0e936156c5595a5524d1ddbc33d8a55a6bd
Robert Knauer <robert@privatdemail.net>
2014-02-13 17:41:43 +0100

    added new cli arguments

--- src/main.cpp
+++ src/main.cpp
@@ -41,12 +41,31 @@ int main(int argc, char **argv)
 	*/
 	gamemode = 13;
 	arg = "";
-	if (argc >= 3)
+	if (argc >= 2)
 	{
-		if (string(argv[1]).compare("load") == 0)
+		if (argc >= 3 && string(argv[1]).compare("load") == 0)
 		{
 			gamemode = 7;
 			arg = argv[2];
+		}
+		else if (argc >= 3 && string(argv[1]).compare("mode") == 0)
+		{
+			gamemode = atoi(argv[2]);
+			if (argc >= 4)
+				arg = argv[3];
+			else
+				arg = "";
+		}
+		else if (string(argv[1]).compare("help") == 0)
+		{
+			std::cout << "== Help Overview ==" << std::endl;
+			std::cout << " -> Showing this help screen:" << std::endl;
+			std::cout << "   " << argv[0] << " help" << std::endl;
+			std::cout << " -> Loading a level file:" << std::endl;
+			std::cout << "   " << argv[0] << " load <filename>" << std::endl;
+			std::cout << " -> Loading a custom gamemode:" << std::endl;
+			std::cout << "   " << argv[0] << " mode <id> [argument]" << std::endl;
+			return EXIT_SUCCESS;
 		};
 	};
 	if (game.init(gamemode, arg) == 1)
